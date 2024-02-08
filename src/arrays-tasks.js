b8582ea1b8e9ea46a0860a3e024d73be18717fce
dmitriy-frostoff <dmitriy.frostoff@yandex.ru>
2024-02-08 17:13:18 +0300

    feat: implement findCommonElements function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -633,8 +633,10 @@ function getMaxItems(arr, n) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  // leave the only arr1 elems that arr2 includes too
+  // no matches ? => return []
+  return arr1.filter((elem) => arr2.includes(elem));
 }
 
 /**
