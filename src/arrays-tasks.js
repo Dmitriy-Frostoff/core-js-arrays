a29d0ff29b2adeebaea2882398169b3ff3e8dc5e
dmitriy-frostoff <dmitriy.frostoff@yandex.ru>
2024-02-08 16:22:55 +0300

    feat: implement getIndicesOfOddNumbers function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -563,8 +563,17 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers.reduce((resArr, num, index) => {
+    // num is odd ? => push it to resArr
+    if (num % 2 !== 0) {
+      resArr.push(index);
+      return resArr;
+    }
+
+    // otherwise return resArr
+    return resArr;
+  }, []);
 }
 
 /**
